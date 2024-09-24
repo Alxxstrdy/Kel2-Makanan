a125a6b112c69eba15e810cdd6c70ea944d8b675
Alxxstrdy <msabilshahputra@gmail.com>
2024-09-24 14:38:32 +0700

    CLASS MAIN

---
+++ src/makanan/Makanan_main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package makanan;
+
+/**
+ *
+ * @author Alxxstrdy
+ */
+public class Makanan_main {
+    
+
+    }
+    
