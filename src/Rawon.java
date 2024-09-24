7826a79630843ec73c6173ea6aac33a10df09d26
mukhlisin <invasijepang45@gmail.com>
2024-09-24 15:31:19 +0700

    Rawon Mukhlisin

---
+++ src/Rawon.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Rawon {
+  String Rasa, Tekstur, Bumbu, Warna, Topping;
+  
+  
+  public void setRasa(String RasaRawon){
+      Rasa = RasaRawon;
+  }
+  
+  public String getRasa(){
+      return Rasa;
+  }
+  
+  public void setTekstur(String TeksturRawon){
+      Tekstur = TeksturRawon;
+  }
+  
+  public String getTekstur(){
+      return Tekstur;
+  }
+  
+  public void setBumbu(String BumbuRawon){
+      Bumbu = BumbuRawon;
+  }
+  
+  public String getBumbu(){
+      return Bumbu;
+  }
+  
+  public void setWarna(String WarnaRawon){
+      Warna = WarnaRawon;
+  }
+  
+  public String getWarna(){
+      return Warna;
+  }
+  
+  public void setTopping(String ToppingRawon){
+      Topping = ToppingRawon;
+  }
+  
+  public String getTopping(){
+      return Topping;
+  }
+ }
