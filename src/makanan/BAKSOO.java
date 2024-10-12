864a817139d269c3490a63b994033394b9e5ccd0
ghina <ghinashofiaaa@gmail.com>
2024-10-12 16:51:08 +0700

    BAKSOO

---
+++ src/makanan/BAKSOO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package makanan;
+
+/**
+ *
+ * @author X1 Carbon
+ */
+public class BAKSOO extends Makanan {
+    String porsi;
+
+    public String getporsi() {
+        return porsi;
+    }
+
+    public void setporsi(String porsi) {
+        this.porsi = porsi;
+    }
+    
+    
+}
+
