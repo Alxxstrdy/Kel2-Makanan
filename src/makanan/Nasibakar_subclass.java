0015d65bf0e76e3077bf9f05b836efd14403f821
nurul <nurulkhoiyah86@gmail.com>
2024-10-12 16:51:28 +0700

    Subclass Nurul

---
+++ src/makanan/Nasibakar_subclass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package makanan;
+
+/**
+ *
+ * @author User
+ */
+public class Nasibakar_subclass extends Makanan{
+    String bungkus;
+    
+    
+    public String getBungkus(){
+        return bungkus;
+    }
+    public void setBungkus(String bungkus){
+        this.bungkus = bungkus;
+    }
+}
