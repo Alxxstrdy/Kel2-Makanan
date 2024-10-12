eb10bdfeec98c26201cd2ff5e740ec9d984c96c7
rifaul <rifaulazhar2@gmail.com>
2024-10-12 16:52:29 +0700

    rifaul subclass

---
+++ src/makanan/rifaulJagungbakar.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package makanan;
+
+/**
+ *
+ * @author rifaul
+ */
+public class rifaulJagungbakar {
+    
+    String jenis;
+
+    public String getjenis() {
+        return jenis;
+    }
+
+    public void setjenis(String jenis) {
+        this.jenis = jenis;
+    }
+    
+    
+}
+   
