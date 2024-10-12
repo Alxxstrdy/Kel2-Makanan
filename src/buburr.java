c9f9fb5fb7a7699cfb0bda5ea094b5b6feb93012
user <user@DESKTOP-TMAM7TH>
2024-10-12 02:53:10 -0700

    buburr bubur M.sulton

---
+++ src/buburr.java
@@ -0,0 +1,27 @@
+
+import makanan.Makanan;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class buburr extends Makanan {
+    
+     String kuah;
+
+    public String getKuah() {
+        return kuah;
+    }
+
+    public void setKuah(String kuah) {
+        this.kuah = kuah;
+    }
+
+
+    
+}
