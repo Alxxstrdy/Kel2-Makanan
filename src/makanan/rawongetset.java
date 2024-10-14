0cc21be05947efa584c1c325d6ae698a0cf94440
ASUS <ASUS@SayMukhlisin>
2024-10-14 21:06:01 +0700

    Pertama dan satu"nya hehe

---
+++ src/makanan/rawongetset.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package makanan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class rawongetset {
+    String khas;
+
+    public String getKhas() {
+        return khas;
+    }
+
+    public void setKhas(String khas) {
+        this.khas = khas;
+    }
+}
