47d5fc8a4df4bfe5ec8b0f31f423276d67f909bc
sangsurya <sangsurya@localhost>
2024-10-14 19:30:51 +0700

    Hasil_NasGor

---
+++ src/makanan/NasGor.java
@@ -0,0 +1,15 @@
+package makanan;
+
+public class NasGor extends Makanan {
+   
+    private String hasil;  
+    
+    public String getHasil() {
+        return hasil;
+    }
+
+    
+    public void setHasil(String hasil) {
+        this.hasil = hasil;
+    }
+}
